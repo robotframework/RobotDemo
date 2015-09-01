a821fc15a4342a998b538a9fe469d866edc9be1d
Pekka Klärck <peke@iki.fi>
2015-09-01 11:02:51 +0300

    fixed and cleaned up package.py

--- package.py
+++ package.py
@@ -1,25 +1,23 @@
 #!/usr/bin/env python
 
-from glob import glob
-from os import chdir, sep as SEP
+from os import getcwd
 from os.path import abspath, dirname, join
 from time import localtime
 from zipfile import ZipFile, ZIP_DEFLATED
+
+assert getcwd() == dirname(abspath(__file__))
 
 NAME = 'RobotDemo'
 ZIP_NAME = NAME + '-%d%02d%02d.zip' % localtime()[:3]
 FILES = ['README.rst',
          'calculator.py',
          'CalculatorLibrary.py',
-         'keyword_driven.txt',
-         'data_driven.txt',
-         'gherkin.txt']
+         'keyword_driven.robot',
+         'data_driven.robot',
+         'gherkin.robot']
 
-chdir(dirname(abspath(__file__)))
-zipfile = ZipFile(ZIP_NAME, 'w', ZIP_DEFLATED)
-for pattern in FILES:
-    for path in glob(pattern.replace('/', SEP)):
+with ZipFile(ZIP_NAME, 'w', ZIP_DEFLATED) as zipfile:
+    for path in FILES:
         print 'Adding:  ', path
         zipfile.write(path, join(NAME, path))
-zipfile.close()
 print 'Created: ', ZIP_NAME
